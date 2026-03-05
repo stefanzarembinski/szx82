9bf90a093da0e40666f52e4af7c4dda2146c32d8
stefanzarembinski <stefan.zarembinski@gmail.com>
2026-03-05 16:10:02 +0100

    update

--- README.md
+++ README.md
@@ -1,24 +1,20 @@
 ## Installation
 
-## INSTALLATION
+### Add the package to the Python path
+
 To build wheel:
-```
-PS .... szx82> python -m build
-```
+`PS .... szx82> python -m build`
+
 To install from wheel:
-```
-PS .... szx82> pip install dist\szx82-0.1-py3-none-any.whl
-```
+`PS .... szx82> pip install dist\szx82-0.1-py3-none-any.whl`
 
 To uninstall:
-```
-PS ... > pip uninstall szx82
-```
+`PS ... > pip uninstall szx82`
 
 To install in editable mode:
-```
-PS ... > szx82> pip install -e .
-```
+`PS ... > szx82> pip install -e .`
 
+### Set the storage directory of the project according to the forex instrument used
 
-
+`PS .... szx81> python -m szx81.config set EURUSD`
+`PS .... szx81> python -m szx81.config set EURUSD-26.10.2024`
--- src/szx82/models/bert/retrains.py
+++ src/szx82/models/bert/retrains.py
@@ -25,112 +25,122 @@ def trainer():
         model=None,
     )
 
-#     training_shell.model = CLS.MODEL
-#     prep='DOWN'
+"""
+    training_shell.model = CLS.MODEL
+    prep='DOWN'
 
-#     training_shell.data = data(
-#         path.join(
-#             r'C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\CLS_DOWN_(0,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\CLS_DOWN_(0,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);.pkl'
-#             ), 
-#         DEVICE, 
-#         scale=1
-#         )
-#     training_shell.pretrained_path = path.join(
-#         r'C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\MlmNsp_DOWN_(0,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\BERT_PRE_DOWN',
-#         'model_bst_.pt'
-# )
-#     training_shell.batch_size = 128
-#     training_shell.stop_thd=0.02
-#     training_shell.init(DEVICE, name_prep=prep, force=False)
-#     training_shell.train()
+    training_shell.data = data(
+        path.join(
+            r'C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\CLS_DOWN_(0,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\CLS_DOWN_(0,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);.pkl'
+            ), 
+        DEVICE, 
+        scale=1
+        )
+    training_shell.pretrained_path = path.join(
+        r'C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\MlmNsp_DOWN_(0,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\BERT_PRE_DOWN',
+        'model_bst_.pt'
+)
+    training_shell.batch_size = 128
+    training_shell.stop_thd=0.02
+    training_shell.init(DEVICE, name_prep=prep, force=False)
+    training_shell.train()
 
-# # train data size: 80000
-# # validation data size: 20000
-# # test data size: 0
-# # vocab hash: ec05018401456653
+# train data size: 80000
+# validation data size: 20000
+# test data size: 0
+# vocab hash: ec05018401456653
 
-# # project dir: C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\MlmNsp_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\BERT_PRE_UP
-# # project name: BERT_PRE_UP
+# project dir: C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\MlmNsp_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\BERT_PRE_UP
+"""
 
-# #  validation; loss trn,val:0.32,0.31; best:122:1.33; train:nsp,mlm:0.68,0.65; val.:nsp,mlm:0.65,0.68; ep:152; batch:156/157;       Training Time: 46920.07 s
+"""
+# project name: BERT_PRE_UP
 
-# # C:\Users\stefa> python -m szx82.models.bert.retrains
+#  validation; loss trn,val:0.32,0.31; best:122:1.33; train:nsp,mlm:0.68,0.65; val.:nsp,mlm:0.65,0.68; ep:152; batch:156/157;       Training Time: 46920.07 s
 
-# # train data size: 160000
-# # validation data size: 39998
-# # test data size: 0
-# # vocab hash: ec05018401456653
-# # Some weights of BertForSequenceClassification were not initialized from the model checkpoint at C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\MlmNsp_DOWN_(0,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\BERT_PRE_DOWN\model_bst_.pt and are newly initialized: ['classifier.bias', 'classifier.weight']
-# # You should probably TRAIN this model on a down-stream task to be able to use it for predictions and inference.
+# train data size: 160000
+# validation data size: 39998
+# test data size: 0
+# vocab hash: ec05018401456653
+# Some weights of BertForSequenceClassification were not initialized from the model checkpoint at C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\MlmNsp_DOWN_(0,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\BERT_PRE_DOWN\model_bst_.pt and are newly initialized: ['classifier.bias', 'classifier.weight']
+# You should probably TRAIN this model on a down-stream task to be able to use it for predictions and inference.
 
-# # project dir: C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\CLS_DOWN_(0,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\BERT_CLS_DOWN
-# # project name: BERT_CLS_DOWN
+# project dir: C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\CLS_DOWN_(0,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\BERT_CLS_DOWN
+"""
 
-# #  "Y" for overwriting existing file in dir C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\CLS_DOWN_(0,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\BERT_CLS_DOWN, anything else to abort: Y
-# #  validation; loss trn,val:0.79,0.88; best:14:0.68; train:acc:0.71; val.:acc:0.68; ep:19; batch:312/313;       Training Time: 4765.18 s
+"""
+# project name: BERT_CLS_DOWN
 
-#     training_shell.model = CLS.MODEL
-#     prep='DOWN'
+#  "Y" for overwriting existing file in dir C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\CLS_DOWN_(0,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\BERT_CLS_DOWN, anything else to abort: Y
+#  validation; loss trn,val:0.79,0.88; best:14:0.68; train:acc:0.71; val.:acc:0.68; ep:19; batch:312/313;       Training Time: 4765.18 s
 
-#     training_shell.data = data(
-#         path.join(
-#             r"C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\CLS_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\CLS_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);.pkl"
-#             ), 
-#         DEVICE, 
-#         scale=1
-#         )
-#     training_shell.pretrained_path = path.join(
-#         r'C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\MlmNsp_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\BERT_PRE_UP',
-#         'model_bst_.pt'
-# )
-#     training_shell.batch_size = 128
-#     training_shell.stop_thd=0.02
-#     training_shell.init(DEVICE, name_prep=prep, force=False)
-#     training_shell.train()
+"""
 
-# # train data size: 160000
-# # validation data size: 39998
-# # test data size: 0
-# # vocab hash: ec05018401456653
-# # Some weights of BertForSequenceClassification were not initialized from the model checkpoint at C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\MlmNsp_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\BERT_PRE_UP\model_bst_.pt and are newly initialized: ['classifier.bias', 'classifier.weight']
-# # You should probably TRAIN this model on a down-stream task to be able to use it for predictions and inference.
+"""
 
-# # project dir: C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\CLS_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\BERT_CLS_DOWN
-# # project name: BERT_CLS_DOWN
+    training_shell.model = CLS.MODEL
+    prep='DOWN'
 
-# #  validation; loss trn,val:0.73,0.79; best:17:0.73; train:acc:0.75; val.:acc:0.73; ep:19; batch:312/313;       Training Time: 4847.73 s
+    training_shell.data = data(
+        path.join(
+            r"C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\CLS_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\CLS_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);.pkl"
+            ), 
+        DEVICE, 
+        scale=1
+        )
+    training_shell.pretrained_path = path.join(
+        r'C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\MlmNsp_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\BERT_PRE_UP',
+        'model_bst_.pt'
+)
+    training_shell.batch_size = 128
+    training_shell.stop_thd=0.02
+    training_shell.init(DEVICE, name_prep=prep, force=False)
+    training_shell.train()
 
+# train data size: 160000
+# validation data size: 39998
+# test data size: 0
+# vocab hash: ec05018401456653
+# Some weights of BertForSequenceClassification were not initialized from the model checkpoint at C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\MlmNsp_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\BERT_PRE_UP\model_bst_.pt and are newly initialized: ['classifier.bias', 'classifier.weight']
+# You should probably TRAIN this model on a down-stream task to be able to use it for predictions and inference.
 
-#     training_shell.model = CLS.MODEL
-#     prep='UP'
+# project dir: C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\CLS_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(0, 302879);\BERT_CLS_DOWN
+# project name: BERT_CLS_DOWN
 
-#     training_shell.data = data(
-#         path.join(
-#             r"C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\CLS_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(297120, 733312);\CLS_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(297120, 733312);.pkl"
-#             ), 
-#         DEVICE, 
-#         scale=1
-#         )
-#     training_shell.pretrained_path = path.join(
-#         r'C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\MlmNsp_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(297120, 733312);\BERT_PRE_UP',
-#         'model_bst_.pt'
-# )
-#     training_shell.batch_size = 128
-#     training_shell.stop_thd=0.02
-#     training_shell.init(DEVICE, name_prep=prep, force=False)
-#     training_shell.train()
+#  validation; loss trn,val:0.73,0.79; best:17:0.73; train:acc:0.75; val.:acc:0.73; ep:19; batch:312/313;       Training Time: 4847.73 s
+"""
 
-# # train data size: 160000
-# # validation data size: 39998
-# # test data size: 0
-# # vocab hash: ec05018401456653
-# # Some weights of BertForSequenceClassification were not initialized from the model checkpoint at C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\MlmNsp_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(297120, 733312);\BERT_PRE_UP\model_bst_.pt and are newly initialized: ['classifier.bias', 'classifier.weight']
-# # You should probably TRAIN this model on a down-stream task to be able to use it for predictions and inference.
+"""
+    training_shell.model = CLS.MODEL
+    prep='UP'
 
-# # project dir: C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\CLS_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(297120, 733312);\BERT_CLS_UP
-# # project name: BERT_CLS_UP
+    training_shell.data = data(
+        path.join(
+            r"C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\CLS_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(297120, 733312);\CLS_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(297120, 733312);.pkl"
+            ), 
+        DEVICE, 
+        scale=1
+        )
+    training_shell.pretrained_path = path.join(
+        r'C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\MlmNsp_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(297120, 733312);\BERT_PRE_UP',
+        'model_bst_.pt'
+)
+    training_shell.batch_size = 128
+    training_shell.stop_thd=0.02
+    training_shell.init(DEVICE, name_prep=prep, force=False)
+    training_shell.train()
 
-# #  validation; loss trn,val:0.83,0.86; best:14:0.70; train:acc:0.71; val.:acc:0.69; ep:19; batch:312/313;       Training Time: 4576.21 s
+# train data size: 160000
+# validation data size: 39998
+# test data size: 0
+# vocab hash: ec05018401456653
+# Some weights of BertForSequenceClassification were not initialized from the model checkpoint at C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\MlmNsp_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(297120, 733312);\BERT_PRE_UP\model_bst_.pt and are newly initialized: ['classifier.bias', 'classifier.weight']
+# You should probably TRAIN this model on a down-stream task to be able to use it for predictions and inference.
+
+# project dir: C:\Users\stefa\Documents\workspaces\szx81\EURUSD\data_store\CLS_UP_(-1,),aligned-l_piecewise;mean_len-15;seg_size-10;idx_step-1;data_range-(297120, 733312);\BERT_CLS_UP
+# project name: BERT_CLS_UP
+
+#  validation; loss trn,val:0.83,0.86; best:14:0.70; train:acc:0.71; val.:acc:0.69; ep:19; batch:312/313;       Training Time: 4576.21 s
 
 
     training_shell.model = CLS.MODEL
@@ -163,6 +173,8 @@ def trainer():
 # project name: BERT_CLS_DOWN
 
 #  validation; loss trn,val:0.84,0.87; best:18:0.66; train:acc:0.67; val.:acc:0.66; ep:19; batch:312/313;       Training Time: 4565.57 s
+
+"""
 
 def main():
     trainer() 
